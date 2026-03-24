6cf4a3c46cf96aa209ecb5708ea003064e6a10a1
KingdomOfFinland <leokoponen567@gmail.com>
2026-03-24 19:34:20 +0200

    Update main.js

--- FISB/main.js
+++ FISB/main.js
@@ -1,11 +1,38 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path'); // Lisää tämä rivi!
 
-function createWindow() {
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch('disable-dev-shm-usage'); // Se muistifiksaus
+app.commandLine.appendSwitch('no-sandbox');
+
+function createWindow () {
   const win = new BrowserWindow({
-    width: 1200, height: 800,
-    webPreferences: { nodeIntegration: true }
+    width: 1280,
+    height: 800,
+    backgroundColor: '#000000',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false 
+    }
   });
-  win.loadFile('index.html');
+
+  // Tämä varmistaa, että polku on oikea riippumatta siitä, mistä ajat komennon
+  win.loadFile(path.join(__dirname, 'index.html')); 
+  
+  win.setMenuBarVisibility(false);
+
+  // iFrame-estojen poisto (sama kuin ennen)
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    let responseHeaders = details.responseHeaders;
+    delete responseHeaders['x-frame-options'];
+    delete responseHeaders['X-Frame-Options'];
+    delete responseHeaders['content-security-policy'];
+    delete responseHeaders['Content-Security-Policy'];
+    callback({ cancel: false, responseHeaders: { ...responseHeaders, 'Access-Control-Allow-Origin': ['*'] } });
+  });
 }
 
 app.whenReady().then(createWindow);
