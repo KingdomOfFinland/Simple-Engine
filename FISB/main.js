2f7e50fed41e65a7d3f43865f5b024fcba2dfefb
KingdomOfFinland <leokoponen567@gmail.com>
2026-03-24 19:45:29 +0200

    Update main.js

--- FISB/main.js
+++ FISB/main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow, session } = require('electron');
-const path = require('path'); // Lisää tämä rivi!
+const path = require('path');
 
+// 1. Estetään rautakiihdytys (auttaa vanhan VA-API:n kanssa)
 app.disableHardwareAcceleration();
 app.commandLine.appendSwitch('disable-gpu');
 app.commandLine.appendSwitch('disable-software-rasterizer');
-app.commandLine.appendSwitch('disable-dev-shm-usage'); // Se muistifiksaus
+app.commandLine.appendSwitch('disable-dev-shm-usage');
 app.commandLine.appendSwitch('no-sandbox');
 
 function createWindow () {
@@ -12,27 +13,40 @@ function createWindow () {
     width: 1280,
     height: 800,
     backgroundColor: '#000000',
+    title: "Fish Browser v0.4.0 | Ultimate",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      // --- TÄMÄ ON SE TÄRKEIN LISÄYS ---
+      webviewTag: true, 
+      // --------------------------------
       webSecurity: false 
     }
   });
 
-  // Tämä varmistaa, että polku on oikea riippumatta siitä, mistä ajat komennon
-  win.loadFile(path.join(__dirname, 'index.html')); 
-  
-  win.setMenuBarVisibility(false);
-
-  // iFrame-estojen poisto (sama kuin ennen)
+  // Poistetaan Header-estot (varmuuden vuoksi, vaikka webview hoitaa useimmat)
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     let responseHeaders = details.responseHeaders;
     delete responseHeaders['x-frame-options'];
     delete responseHeaders['X-Frame-Options'];
     delete responseHeaders['content-security-policy'];
     delete responseHeaders['Content-Security-Policy'];
-    callback({ cancel: false, responseHeaders: { ...responseHeaders, 'Access-Control-Allow-Origin': ['*'] } });
+
+    callback({
+      cancel: false,
+      responseHeaders: {
+        ...responseHeaders,
+        'Access-Control-Allow-Origin': ['*']
+      }
+    });
   });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+  win.setMenuBarVisibility(false);
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
